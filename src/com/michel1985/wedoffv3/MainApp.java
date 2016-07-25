101f1ea32600ccdb3329c8c98008b91871261682
1985michel <1985michel@live.com>
2016-07-24 23:48:15 -0300

    - Criado AboutDialog; - Criado alteraro RootLayoutController - Alterado RootLayout.fxml - Alterada a classe MainApp.
    Com este commit o menu superior da aplicação (root) provê as
    funcionalidades de exit [ file/Fechar e Sair] e about [ help/Soube
    WeDoFF]
    
    As libs de Dialogs foram excluidas vez que não estavam operando
    adequadamente e suas funcionalidades são sobrepostas por Alert e Dialog
    criados manualmente.

--- src/com/michel1985/wedoffv3/MainApp.java
+++ src/com/michel1985/wedoffv3/MainApp.java
@@ -1,136 +1,164 @@
 package com.michel1985.wedoffv3;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 import com.michel1985.wedoffv3.model.Cliente;
 import com.michel1985.wedoffv3.model.Usuario;
 import com.michel1985.wedoffv3.view.AtendendoClienteOverviewController;
 import com.michel1985.wedoffv3.view.LoginOverviewController;
+import com.michel1985.wedoffv3.view.RootLayoutController;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.DialogPane;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-	
+
 	private Stage primaryStage;
 	private BorderPane rootLayout;
-	
+
 	private ObservableList<Cliente> clienteData = FXCollections.observableArrayList();
-	
+
 	private Usuario usuarioAtivo;
-	
 
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("WeDoFF V3.0");
-		
+
 		// Set the application icon.
 		this.primaryStage.getIcons().add(new Image("file:resources/images/security-app-shield-icon.png"));
-		
+
 		initRootLayout();
-		
+
 		showLoginOverview();
-		//showAtendendoClienteOverview();
+		// showAtendendoClienteOverview();
 	}
-	
+
 	/**
 	 * Inicializa o rootLayout(base)
-	 * */
-	public void initRootLayout(){
-		try{
-			//carrega root fxml
+	 */
+	public void initRootLayout() {
+		try {
+			// carrega root fxml
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
 			rootLayout = (BorderPane) loader.load();
-			
-			//Colocando o RootLayout em cena
-			primaryStage.setScene( new Scene(rootLayout));
+
+			// Give the controller access to the main app.
+			RootLayoutController controller = loader.getController();
+			controller.setMainApp(this);
+
+			// Colocando o RootLayout em cena
+			primaryStage.setScene(new Scene(rootLayout));
 			primaryStage.show();
-		}catch(IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Colocando o LoginOverview dentro do RootLayout
-	 * */
-	public void showLoginOverview(){
-		try{
-			//Passo 1 - Carregando FXML
+	 */
+	public void showLoginOverview() {
+		try {
+			// Passo 1 - Carregando FXML
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource(LoginOverviewController.location));
 			AnchorPane loginOverview = (AnchorPane) loader.load();
-			
-			//Colocando dentro do root
+
+			// Colocando dentro do root
 			rootLayout.setCenter(loginOverview);
-			
-			//Dando acesso para o controller acessar o main
+
+			// Dando acesso para o controller acessar o main
 			LoginOverviewController controller = loader.getController();
 			controller.setMainApp(this);
-			
-		}catch(IOException e){
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Colocando o AtendendoCLiente dentro do RootLayout
-	 * */
-	public void showAtendendoClienteOverview(){
-		try{
-			//Passo 1 - Carregandoo FXML
+	 */
+	public void showAtendendoClienteOverview() {
+		try {
+			// Passo 1 - Carregandoo FXML
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource("view/AtendendoClienteOverview.fxml"));
 			AnchorPane atendendoClienteOverview = (AnchorPane) loader.load();
-			
-			//Colocando dentro do root
+
+			// Colocando dentro do root
 			rootLayout.setCenter(atendendoClienteOverview);
-			
-			//Dando acesso para o controller acessar a MainApp
+
+			// Dando acesso para o controller acessar a MainApp
 			AtendendoClienteOverviewController controller = loader.getController();
 			controller.setMainApp(this);
-		
-		}catch(IOException e){
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
-	
+
+	public void showAboutDialog() {
+		try {
+			// Load o FXML
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/AboutDialog.fxml"));
+			DialogPane page = (DialogPane) loader.load();
+
+			// Criando o dialogStage
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Sobre o WeDoFF");
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.initOwner(primaryStage);
+			// dialogStage.getIcons().add(new
+			// Image("file:resources/images/edit.png"));
+			Scene scene = new Scene(page);
+			dialogStage.setScene(scene);
+
+			// Show
+			dialogStage.showAndWait();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * Retorna o palco principal
-	 * */
-	public Stage getPrimaryStage(){
+	 */
+	public Stage getPrimaryStage() {
 		return primaryStage;
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	/**
 	 * Obterndo as ObservableList
-	 * */
-	public ObservableList<Cliente> getClienteData(){
+	 */
+	public ObservableList<Cliente> getClienteData() {
 		return this.clienteData;
 	}
-	
+
 	/**
 	 * Setando o usuário ativo
-	 * */
-	public void setUsuarioAtivo(Usuario user){
+	 */
+	public void setUsuarioAtivo(Usuario user) {
 		this.usuarioAtivo = user;
 	}
-	
-	
-	
+
 }
---
+++ src/com/michel1985/wedoffv3/view/RootLayoutController.java
@@ -0,0 +1,53 @@
+package com.michel1985.wedoffv3.view;
+
+import com.michel1985.wedoffv3.MainApp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Menu;
+
+public class RootLayoutController {
+
+	private MainApp mainApp;
+
+	/**
+	 * setMainApp - é usado pelo MainApp para para se referenciar
+	 */
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+
+		// Em casos de tabela,aqui é o local para solitiar o povoamento
+		// someTable.setItems(mainApp.getClienteData());
+	}
+
+	// Contrutor. É chamado antes do método Initialize
+	public RootLayoutController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@FXML // This method is called by the FXMLLoader when initialization is
+			// complete
+	void initialize() {
+
+	}
+
+	@FXML
+	private Menu configuracoesMenu;
+
+	/**
+	 * Abre uma Janela com informações sobre o Sistema
+	 */
+	@FXML
+	private void handleAbout() {
+		mainApp.showAboutDialog();
+	}
+
+	/**
+	 * Fecha a aplicação
+	 */
+	@FXML
+	private void handleExit() {
+		System.exit(0);
+	}
+
+}
