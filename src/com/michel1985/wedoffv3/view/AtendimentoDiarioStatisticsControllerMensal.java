8ca637d09fd49751c21413d8cd004c719e3fbf0a
1985michel <1985michel@live.com>
2016-09-16 21:14:55 -0300

    A aplicação é capaz de apresentar as estatísticas mensais de atendimento

--- src/com/michel1985/wedoffv3/view/AtendimentoDiarioStatisticsControllerMensal.java
+++ src/com/michel1985/wedoffv3/view/AtendimentoDiarioStatisticsControllerMensal.java
@@ -1,6 +1,5 @@
 package com.michel1985.wedoffv3.view;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
@@ -35,108 +34,51 @@ public class AtendimentoDiarioStatisticsControllerMensal {
 	private void initialize() {
 	}
 
-	/**
-	 * Ligando ao main
-	 */
 	public void setMainApp(MainApp main) {
 		this.mainApp = main;
 	}
 
-	// Coletando todas as datas em que houve atendimento
-	private ArrayList<String> pegaTodasAsDatasDosAtendimentos() {
-
-		ObservableList<Atendimento> listaAtd = mainApp.getAtendimentoData();
-		ArrayList<String> datas = new ArrayList<>();
-
-		for (Atendimento atd : listaAtd) {
-			if (atd != null && isAnoCorrente(atd.getDataAtendimento())) {
-
-				int valor = datasEValores.get(estruturaData(atd.getDataAtendimento())[1]);
+	// Criando a legenda do gráfico: os meses
+	private void criandoOsMeses() {
+		for (int i = 0; i <= 12; i++) {
+			meses.add(i + "");
+		}
+	}
+	
+	private void colocandoOsMesesComoKeys(){
+		datasEValores = new HashMap<String, Integer>();
+		datasEQtdAgendamentos = new HashMap<String, Integer>();
+		
+		for(String mes : meses){
+			datasEValores.put(mes,0);
+			datasEQtdAgendamentos.put(mes,0);
+		}
+	}
+		
+	//Pegando todos os atendimentos e separando-os por mes
+	private void categorizaAtendimentosPorMes(){		
+		
+		for(Atendimento atd: mainApp.getAtendimentoData()){
+			String mes = (estruturaData(atd.getDataAtendimento())[1])+"";
+			int valor = datasEValores.get(mes);
+			valor++;
+			datasEValores.replace(mes, valor);
+		}
+	}
+	
+	//Pegando todos os agendamentos e separando-os por mês
+	private void categorizaAgendaimentosPorMes(){
+		for(Atendimento atd: mainApp.getAtendimentoData()){
+			if(atd.getIsAgendamento()){
+				String mes = (estruturaData(atd.getDataAtendimento())[1])+"";
+				int valor = datasEQtdAgendamentos.get(mes);
 				valor++;
-				datasEValores.replace(estruturaData(atd.getDataAtendimento())[1] + "", valor);
-
-				if (atd.getIsAgendamento()) {
-					int qtdAgendamentos = datasEQtdAgendamentos.get(estruturaData(atd.getDataAtendimento())[1]);
-					qtdAgendamentos++;
-					datasEQtdAgendamentos.replace(estruturaData(atd.getDataAtendimento())[1] + "", qtdAgendamentos);
-				}
-
-			}
+				datasEQtdAgendamentos.replace(mes, valor);				
+			}			
 		}
-
-		return datas;
 	}
-
-	private boolean jaTemAData(ArrayList<String> datas, String data) {
-		boolean tem = false;
-		for (String dt : datas) {
-			if (dt.equalsIgnoreCase(data))
-				tem = true;
-		}
-
-		return tem;
-	}
-
-	private boolean jaTemADataAgendamento(HashMap<String, Integer> map, String data) {
-		boolean tem = false;
-		Set<String> datas = map.keySet();
-		for (String dt : datas) {
-			if (dt.equalsIgnoreCase(data))
-				tem = true;
-		}
-
-		return tem;
-	}
-
-	public void setAndShowData() {
-
-		// Inicializando as listas
-		datasEValores = new HashMap<>();
-		datasEQtdAgendamentos = new HashMap<>();
-
-		// Criando o eixo x com os números dos meses
-		ArrayList<String> diasArray = new ArrayList<>();
-		for (int i = 1; i <= 12; i++) {
-			diasArray.add(i + "");
-		}
-		meses.addAll(diasArray);
-		xAxis.setCategories(meses);
-
-		// Criando as colunas exibidas nos gráficos
-		XYChart.Series<String, Integer> series = new XYChart.Series<>();
-		series.setName("Atendimentos");
-		XYChart.Series<String, Integer> seriesAgendamentos = new XYChart.Series<>();
-		seriesAgendamentos.setName("Agendamentos");
-
-		// colocando os Meses como Keys nos HashMaps
-		for (String dia : meses) {
-			datasEValores.clear();
-			datasEValores.put(dia, 0);
-			datasEQtdAgendamentos.clear();
-			datasEQtdAgendamentos.put(dia, 0);
-		}
-
-		// Varrendo os atendimentos, coletando os dados e colocando nos maps
-		pegaTodasAsDatasDosAtendimentos();
-
-		Set<String> keys = datasEValores.keySet();
-
-		for (String key : keys) {
-
-			series.getData().add(new XYChart.Data<>(key + "", datasEValores.get(key)));
-			if (datasEQtdAgendamentos.containsKey(key))
-				seriesAgendamentos.getData()
-						.add(new XYChart.Data<>(key + "", datasEQtdAgendamentos.get(key)));
-
-		}
-
-		barChart.getData().add(series);
-		barChart.getData().add(seriesAgendamentos);
-	}
-
-	// metodo que recebe uma String baseadas em LocalDate e retorna o ano, mes e
-	// dia
-	public int[] estruturaData(String data) {
+	
+	private int[] estruturaData(String data) {
 
 		String[] dataStr = data.split("-");
 		int ano = Integer.parseInt(dataStr[0]);
@@ -145,15 +87,41 @@ public class AtendimentoDiarioStatisticsControllerMensal {
 
 		return new int[] { ano, mes, dia };
 	}
+	
+	private void doTheStatisticWork(){
+		criandoOsMeses();
+		colocandoOsMesesComoKeys();
+		categorizaAtendimentosPorMes();
+		categorizaAgendaimentosPorMes();
+	}
+	
+	public void setAndShowData() {
 
-	public boolean isAnoCorrente(String data) {
+		doTheStatisticWork();
+		
+		xAxis.setCategories(meses);
 
-		int anoHoje = 2016;
+		XYChart.Series<String, Integer> series = new XYChart.Series<>();
+		series.setName("Atendimentos");
+		
+		XYChart.Series<String, Integer> seriesAgendamentos = new XYChart.Series<>();
+		seriesAgendamentos.setName("Agendamentos");
+		
+		barChart.setBarGap(1);
 
-		if (anoHoje == estruturaData(data)[2])
-			return true;
-		else
-			return false;
+		Set<String> keys = datasEValores.keySet();
+
+		for (String key : keys) {
+			
+			series.getData().add(new XYChart.Data<>(key, datasEValores.get(key)));
+			seriesAgendamentos.getData().add(new XYChart.Data<>(key, datasEQtdAgendamentos.get(key)));
+
+		}
+		
+	
+
+		barChart.getData().add(series);
+		barChart.getData().add(seriesAgendamentos);
 	}
 
 }
